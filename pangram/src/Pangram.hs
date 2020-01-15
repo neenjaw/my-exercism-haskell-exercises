4adf99d789f9e674ae24b65a4137a9543b1ae18f
Tim Austin <tim@neenjaw.com>
2020-01-15 16:52:00 -0600

    soln

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,4 +1,8 @@
 module Pangram (isPangram) where
 
+import qualified Data.Set as Set
+import qualified Data.Char as Char
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text = Set.size textSet == 26
+  where textSet = Set.fromList [ l | c <- text, let l = Char.toLower c, l `elem` ['a'..'z']]
