a4aa6315a1912fbc4ef54cbbaf454fb5dd5ffc5d
Tim Austin <tim@neenjaw.com>
2020-01-14 21:03:49 -0600

    pangram start

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -9,5 +9,18 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
+secondsPerEarthYear :: Float
+secondsPerEarthYear = 31557600.0
+
+relativeYear :: Planet -> Float
+relativeYear Mercury = 0.2408467
+relativeYear Venus   = 0.61519726
+relativeYear Earth   = 1.0
+relativeYear Mars    = 1.8808158
+relativeYear Jupiter = 11.862615
+relativeYear Saturn  = 29.447498
+relativeYear Uranus  = 84.016846
+relativeYear Neptune = 164.79132
+
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = seconds / secondsPerEarthYear / (relativeYear planet)
