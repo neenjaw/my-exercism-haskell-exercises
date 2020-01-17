e2ed4bf0b48eecb6d78970a5bbbbac0b212f1bbd
Tim Austin <tim@neenjaw.com>
2020-01-17 17:20:41 -0600

    bob soln

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,4 +1,17 @@
 module Bob (responseFor) where
 
-responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+import qualified Data.Text as T
+import           Data.Text (Text)
+
+responseFor :: Text -> Text
+responseFor t
+  | isQuestion && isYelling = T.pack "Calm down, I know what I'm doing!"
+  | isQuestion              = T.pack "Sure."
+  | isYelling               = T.pack "Whoa, chill out!"
+  | isBlank                 = T.pack "Fine. Be that way!"
+  | otherwise               = T.pack "Whatever."
+  where stripped   = T.strip t
+        hasAlpha   = T.toLower stripped /= T.toUpper stripped
+        isBlank    = T.pack "" == stripped
+        isQuestion = T.pack "?" `T.isSuffixOf` stripped
+        isYelling  = T.toUpper stripped == stripped && hasAlpha
