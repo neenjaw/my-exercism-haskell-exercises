ad9d3dec282a149fe96baf875f1ecf15caa867df
Tim Austin <tim@neenjaw.com>
2020-01-18 15:46:30 -0600

    now with overloaded strings

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bob (responseFor) where
 
 import qualified Data.Text as T
@@ -5,13 +7,13 @@ import           Data.Text (Text)
 
 responseFor :: Text -> Text
 responseFor t
-  | isQuestion && isYelling = T.pack "Calm down, I know what I'm doing!"
-  | isQuestion              = T.pack "Sure."
-  | isYelling               = T.pack "Whoa, chill out!"
-  | isBlank                 = T.pack "Fine. Be that way!"
-  | otherwise               = T.pack "Whatever."
+  | isQuestion && isYelling = "Calm down, I know what I'm doing!"
+  | isQuestion              = "Sure."
+  | isYelling               = "Whoa, chill out!"
+  | isBlank                 = "Fine. Be that way!"
+  | otherwise               = "Whatever."
   where stripped   = T.strip t
         hasAlpha   = T.toLower stripped /= T.toUpper stripped
-        isBlank    = T.pack "" == stripped
-        isQuestion = T.pack "?" `T.isSuffixOf` stripped
+        isBlank    = "" == stripped
+        isQuestion = "?" `T.isSuffixOf` stripped
         isYelling  = T.toUpper stripped == stripped && hasAlpha
