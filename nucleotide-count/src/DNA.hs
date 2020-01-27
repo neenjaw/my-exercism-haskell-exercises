24e7ab06de0af6ef2eaea27de48e662de439df03
Tim Austin <tim@neenjaw.com>
2020-01-27 10:59:35 -0600

    soln

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,8 +1,32 @@
-module DNA (nucleotideCounts, Nucleotide(..)) where
+{-# LANGUAGE TupleSections #-}
 
-import Data.Map (Map)
+module DNA (nucleotideCounts, Nucleotide(..), newHistogram) where
+
+import qualified Data.Map as M
+import           Data.Map (Map)
+import           Control.Monad (foldM)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+type Histogram = Map Nucleotide Int
+
+newHistogram :: Histogram
+newHistogram = M.fromList $ map (, 0) [A, C, G, T]
+
+nucleotideCounts :: String -> Either String Histogram
+nucleotideCounts xs =
+  case foldM updateCount h xs of
+    Just result -> Right result
+    Nothing -> Left xs
+  where
+    h = newHistogram
+
+updateCount :: Histogram -> Char -> Maybe Histogram
+updateCount h 'A' = Just $ doUpdateCount h A
+updateCount h 'C' = Just $ doUpdateCount h C
+updateCount h 'T' = Just $ doUpdateCount h T
+updateCount h 'G' = Just $ doUpdateCount h G
+updateCount _ _ = Nothing
+
+doUpdateCount :: Histogram -> Nucleotide -> Histogram
+doUpdateCount h n = M.adjust succ n h
